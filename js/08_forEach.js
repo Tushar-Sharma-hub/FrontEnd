63e4ba61165af87c0e20619b233b0c3bbc64edc9
Tushar-Sharma-hub <ts817479@gmail.com>
2025-09-19 12:48:39 +0000

    update

---
+++ js/08_forEach.js
@@ -0,0 +1,4 @@
+var a=[1,2,3,4,5];
+a.forEach(function(val){ //only applied for arrray.
+    console.log(val+2); //It doesn't change og array
+})
---
+++ js/09_forIn.js
@@ -0,0 +1,7 @@
+var obj={
+    name:"tushar",
+    age:19
+};
+for(var key in obj){
+    console.log(key,obj[key]);
+}
